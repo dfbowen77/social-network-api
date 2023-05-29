8efb70e7193e2063fc8c74b090aa1b562d693c09
Dan Bowen <danbowen2677@gmail.com>
2023-05-29 12:38:58 -0400

    added code to the api folder under routes to define the api endpoints for myriad routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+// create a new router object
+const router = require('express').Router();
+
+// import all the necessary controllers from the controllers folder
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController.js');
+
+// sets up the endpoint for retreiving and adding users
+router.route('/').get(getUsers).post(createUser);
+
+// sets up the endpoints for retreiving, updating, and deleting single users
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// sets up the endpoints for adding and deleting friends
+router.route('.:userID/friends/:friendID').post(addFriend).delete(removeFriend)
+
+// Exports the router for use elsewhere in the application
+module.exports = router;
