0fb238bfe6af997b2f2cfd7c0f056027826c0bcd
Dan Bowen <danbowen2677@gmail.com>
2023-05-30 10:50:41 -0400

    added code to the user controller file to define what happens at the various user endpoints

---
+++ controllers/userController.js
@@ -0,0 +1,114 @@
+// import the destructured user model
+const { User } = require('../models');
+
+// Creates the User Controller for handling myriad user routes
+
+const UserController = {
+    // Get all Users
+    async getUsers(req, res) {
+        try {
+          const users = await User.find();    
+          res.json(users);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+      },
+    //   Get a single user
+    async getSingleUser(req, res) {
+        try {
+          const user = await User.findOne({ _id: req.params.userId })
+    
+          if (!user) {
+            return res.status(404).json({ message: 'No user with that ID' })
+          }
+          res.json(user);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+    },  
+    // create a new user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // update a user
+    async updateUser(req, res) {
+        try {
+            const user  = await User.findOneAndUpdate(req.params.id, req.body, {new: true})
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' })
+            }
+            res.json(user)
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+          }
+    },
+    // Delete a user
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndRemove({_id: req.params.userId})
+
+            if (!user) {
+                return res.status(404).json({ message: 'No such user exists' });
+              }
+        
+              res.json({ message: 'User successfully deleted' });
+            } catch (err) {
+              console.log(err);
+              res.status(500).json(err);
+            }
+    },
+    // Add a Friend to a User
+    async addFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.body.friendId || req.params.friendId } },
+                { runValidators: true, new: true }
+              );
+        
+              if (!user) {
+                return res
+                  .status(404)
+                  .json({ message: 'No user found with that ID :(' });
+              }
+        
+              res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+          }
+    },
+    // Delete a friend from a user
+    async removeFriend(req, res) {
+        try {
+          const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friend: { friendId: req.params.friendId } } },
+            { runValidators: true, new: true }
+          );
+    
+          if (!user) {
+            return res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' });
+          }
+    
+          res.json(user);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      },
+
+}
+
+// Exports the User Controller for use elsewhere within the application
+module.export = UserController
